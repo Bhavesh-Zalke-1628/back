01ae623e278fbe834ee1d9711e32baf8be95a163
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-08-24 15:40:30 +0530

    Add data

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { config } from "dotenv"
 config()
 const PORT = process.env.PORT
 console.log(PORT)
+
+app.get('/twit', (req, res) => {
+    res.send("hello i am bhavesh ")
+    res.send('good by')
+})
 app.listen(PORT, async () => {
     console.log("The localhost start")
 })
